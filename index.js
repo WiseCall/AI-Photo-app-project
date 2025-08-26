98717b18fa60c622bc6d2318d392f981d5889827
Callum Wiseman <wisecall18@outlook.com>
2025-08-26 21:22:15 +0100

    Added an EventListener to store the desired search input to a variable.

---
+++ index.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById('search-input')
+const searchButton = document.getElementById('search-button')
+
+
+
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value
+})
