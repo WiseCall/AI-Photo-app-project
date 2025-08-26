07ed65480c4ea56660c12dd325b38a1c0f845d05
Callum Wiseman <wisecall18@outlook.com>
2025-08-26 22:15:08 +0100

    Added functionality to supply the webpage with the user's images.

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const searchInput = document.getElementById('search-input')
 const searchButton = document.getElementById('search-button')
+const fileInput = document.getElementById('file-input')
+const display = document.getElementById('photo-display')
+
 
 
 
 searchButton.addEventListener('click', () => {
     const searchTerm = searchInput.value
+})
+
+fileInput.addEventListener('change', () => {
+    display.innerHTML = ""
+    const files = fileInput.files
+      
+    Array.from(files).forEach(file => {
+        const reader = new FileReader()
+        reader.onload = e => {
+            const itemDiv = document.createElement('div')
+            itemDiv.className = 'item'
+            const img = document.createElement('img')
+            img.src = e.target.result
+            itemDiv.appendChild(img)
+            display.appendChild(itemDiv)
+        }
+        reader.readAsDataURL(file)
+    })
 })
